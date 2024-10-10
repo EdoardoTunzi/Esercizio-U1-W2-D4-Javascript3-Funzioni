5a459ee01e2883161f7eb394733a47fbf1a90213
Edoardo <edfl.tunzi@gmail.com>
2024-10-10 16:25:33 +0200

    type safety checks added to all functions

--- js/D4.js
+++ js/D4.js
@@ -18,17 +18,25 @@ console.log("L'area del rettangolo è uguale a:", area(2, 4));
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 function crazySum(num1, num2) {
-  if (num1 === num2) {
-    console.log(num1, "e", num2, "sono uguali, la somma è stata moltiplicata per 3");
-    return (num1 + num2) * 3;
+  let total = 0;
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    if (num1 === num2) {
+      total = (num1 + num2) * 3;
+      console.log(num1, "e", num2, "sono uguali, la somma è stata moltiplicata per 3, per un totale di", total);
+      return;
+    } else {
+      total = num1 + num2;
+      console.log(num1, "e", num2, "non sono uguali, la loro somma è", total);
+      return;
+    }
   } else {
-    console.log(num1, "e", num2, "non sono uguali, la loro somma ");
-    return num1 + num2;
+    console.log("I parametri forniti non sono di tipo numerico");
   }
 }
 
-console.log(crazySum(2, 2));
-console.log(crazySum(5, 2));
+crazySum(2, 2);
+crazySum(5, 2);
+//crazySum("ciao", true);
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
@@ -38,15 +46,20 @@ console.log(crazySum(5, 2));
 function crazyDiff(num1) {
   let absResult = 0;
   let absMult = 0;
-  if (num1 > 19) {
-    absResult = Math.abs(num1 - 19);
-    absMult = absResult * 3;
-    console.log("il parametro ", num1, "è superiore a 19, la differenza assoluta è", absResult, "il risultato è stato moltiplicato per 3 ed è", absMult);
-    return;
+  if (typeof num1 === "number") {
+    if (num1 > 19) {
+      absResult = Math.abs(num1 - 19);
+      absMult = absResult * 3;
+      console.log("il parametro ", num1, "è superiore a 19, la differenza assoluta è", absResult, "il risultato è stato moltiplicato per 3 ed è", absMult);
+      return;
+    } else {
+      absResult = Math.abs(num1 - 19);
+      console.log("il parametro", num1, "è inferiore a 19,la differenza assoluta è", absResult);
+      return;
+    }
+  } else {
+    console.log("I parametri forniti non sono di tipo numerico");
   }
-  absResult = Math.abs(num1 - 19);
-  console.log("il parametro", num1, "è inferiore a 19,la differenza assoluta è", absResult);
-  return;
 }
 
 crazyDiff(30);
@@ -60,17 +73,22 @@ crazyDiff(17);
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 function boundary(n) {
-  if (n >= 20 && n <= 100) {
-    console.log(n, "è incluso tra 20 e 100");
-    return;
-  } else if (n === 400) {
-    console.log(n, "è uguale a 400");
-    return;
+  if (typeof n === "number") {
+    if (n >= 20 && n <= 100) {
+      console.log(n, "è incluso tra 20 e 100");
+      return;
+    } else if (n === 400) {
+      console.log(n, "è uguale a 400");
+      return;
+    } else {
+      console.log(n, "non è incluso tra 20 e 100, e non è uguale a 400");
+      return;
+    }
   } else {
-    console.log(n, "non è incluso tra 20 e 100, e non è uguale a 400");
-    return;
+    console.log("I parametri forniti non sono di tipo numerico");
   }
 }
+
 boundary(35);
 
 /* ESERCIZIO 5
@@ -81,15 +99,19 @@ boundary(35);
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 function epify(string) {
-  let word = string;
+  if (typeof string === "string") {
+    let word = string;
 
-  if (word.startsWith("Epicode")) {
-    console.log(word);
-    return;
+    if (word.startsWith("Epicode")) {
+      console.log(word);
+      return;
+    } else {
+      word = "Epicode" + " " + string;
+      console.log(word);
+      return;
+    }
   } else {
-    word = "Epicode" + " " + string;
-    console.log(word);
-    return;
+    console.log("Il parametro fornito non è una stringa");
   }
 }
 epify("che bello");
@@ -101,12 +123,16 @@ epify("Epicode");
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 function check3and7(num) {
-  if (num % 3 === 0 || num % 7 === 0) {
-    console.log(num, "è multiplo di 3 o 7");
-    return;
+  if (typeof num === "number") {
+    if (num % 3 === 0 || num % 7 === 0) {
+      console.log(num, "è multiplo di 3 o 7");
+      return;
+    } else {
+      console.log(num, "non è un multiplo di 3 o 7");
+      return;
+    }
   } else {
-    console.log(num, "non è un multiplo di 3 o 7");
-    return;
+    console.log("I parametri forniti non sono di tipo numerico");
   }
 }
 check3and7(22);
@@ -117,11 +143,15 @@ check3and7(22);
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 function reverseString(string) {
-  let word = string;
-  word = word.split("");
-  word = word.reverse();
-  console.log(word.join(""));
-  return;
+  if (typeof string === "string") {
+    let word = string;
+    word = word.split("");
+    word = word.reverse();
+    console.log(word.join("").toUpperCase());
+    return;
+  } else {
+    console.log("Il parametro fornito non è una stringa");
+  }
 }
 reverseString("Epicode");
 /* ESERCIZIO 8
@@ -130,7 +160,9 @@ reverseString("Epicode");
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-
+function upperFirst(string) {
+  let word = string;
+}
 /* ESERCIZIO 9
  Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
  della stringa originale.
